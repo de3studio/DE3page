ef509cb35d3990811f0c17c71ca46d90aa3a0484
Your Name <you@example.com>
2023-02-19 15:35:47 +0000

    dgfdf

---
+++ assets/3D-4f60a4d1.js
@@ -0,0 +1,148 @@
+import{a as l}from"./axios-a900fd7e.js";import{_ as n,r as c,o as p,c as f,a as e,b as s,w as o,F as h,d as a,e as r}from"./index-02ff7b65.js";const g={methods:{gefe(){this.iframeString=`<iframe
+          style="
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            border: 0;
+          "
+          srcdoc="<style>
+          * {
+          padding: 0;
+          margin: 0;
+          overflow: hidden;
+          }
+          
+          body, html {
+            height: 100%;
+          }
+          
+          img, svg {
+            position: absolute;
+            width: 100%;
+            top: 0;
+            bottom: 0;
+            margin: auto;
+          }
+          
+          svg {
+            filter: drop-shadow(1px 1px 10px hsl(206.5, 70.7%, 8%));
+            transition: all 250ms ease-in-out;
+          }
+          
+          body:hover svg {
+            filter: drop-shadow(1px 1px 10px hsl(206.5, 0%, 10%));
+            transform: scale(1.2);
+          }
+        </style>
+        <a href='https://www.youtube.com/embed/${this.imgSrc}?autoplay=1'>
+          <img src='https://img.youtube.com/vi/${this.imgSrc}/maxresdefault.jpg'  alt='Coffee Recipe Javascript Project'>
+          <svg xmlns='http://www.w3.org/2000/svg' width='64' height='64' viewBox='0 0 24 24' fill='none' stroke='#ffffff' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-play-circle'><circle cx='12' cy='12' r='10'></circle><polygon points='10 8 16 12 10 16 10 8'></polygon></svg>
+        </a>
+        "
+          src="https://www.youtube.com/embed/${this.imgSrc}"
+          title="Coffee Recipe Javascript Project"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        >
+        </iframe>`,this.iframeString3=`<iframe
+          style="
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            border: 0;
+          "
+          srcdoc="<style>
+          * {
+          padding: 0;
+          margin: 0;
+          overflow: hidden;
+          }
+          
+          body, html {
+            height: 100%;
+          }
+          
+          img, svg {
+            position: absolute;
+            width: 100%;
+            top: 0;
+            bottom: 0;
+            margin: auto;
+          }
+          
+          svg {
+            filter: drop-shadow(1px 1px 10px hsl(206.5, 70.7%, 8%));
+            transition: all 250ms ease-in-out;
+          }
+          
+          body:hover svg {
+            filter: drop-shadow(1px 1px 10px hsl(206.5, 0%, 10%));
+            transform: scale(1.2);
+          }
+        </style>
+        <a href='https://www.youtube.com/embed/${this.imgSrc3}?autoplay=1'>
+          <img src='https://img.youtube.com/vi/${this.imgSrc3}/maxresdefault.jpg'  alt='Coffee Recipe Javascript Project'>
+          <svg xmlns='http://www.w3.org/2000/svg' width='64' height='64' viewBox='0 0 24 24' fill='none' stroke='#ffffff' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-play-circle'><circle cx='12' cy='12' r='10'></circle><polygon points='10 8 16 12 10 16 10 8'></polygon></svg>
+        </a>
+        "
+          src="https://www.youtube.com/embed/${this.imgSrc3}"
+          title="Coffee Recipe Javascript Project"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        >
+        </iframe>`,this.iframeString2=`<iframe
+          style="
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            border: 0;
+          "
+          srcdoc="<style>
+          * {
+          padding: 0;
+          margin: 0;
+          overflow: hidden;
+          }
+          
+          body, html {
+            height: 100%;
+          }
+          
+          img, svg {
+            position: absolute;
+            width: 100%;
+            top: 0;
+            bottom: 0;
+            margin: auto;
+          }
+          
+          svg {
+            filter: drop-shadow(1px 1px 10px hsl(206.5, 70.7%, 8%));
+            transition: all 250ms ease-in-out;
+          }
+          
+          body:hover svg {
+            filter: drop-shadow(1px 1px 10px hsl(206.5, 0%, 10%));
+            transform: scale(1.2);
+          }
+        </style>
+        <a href='https://www.youtube.com/embed/${this.imgSrc2}?autoplay=1'>
+          <img src='https://img.youtube.com/vi/${this.imgSrc2}/maxresdefault.jpg'  alt='Coffee Recipe Javascript Project'>
+          <svg xmlns='http://www.w3.org/2000/svg' width='64' height='64' viewBox='0 0 24 24' fill='none' stroke='#ffffff' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-play-circle'><circle cx='12' cy='12' r='10'></circle><polygon points='10 8 16 12 10 16 10 8'></polygon></svg>
+        </a>
+        "
+          src="https://www.youtube.com/embed/${this.imgSrc2}"
+          title="Coffee Recipe Javascript Project"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        >
+        </iframe>`}},components:{},async mounted(){window.scrollTo({top:0});const d=await l.get("https://loquacious-crepe-a6721d.netlify.app/.netlify/functions/api/");this.data=d.data.vedios,this.imgSrc=this.data.vedios,this.imgSrc2=this.data.vedio2,this.imgSrc3=this.data.vedio3,this.gefe()},data(){return{data:[],imgSrc:"LFWWLvkB3mE",imgSrc2:"LFWWLvkB3mE",imgSrc3:"LFWWLvkB3mE",iframeString:"",iframeString2:"",iframeString3:""}}},m={class:"dgdfg2",style:{"margin-top":"0"}},u={class:"fgffg"},v={style:{"border-radius":"20px",position:"relative","padding-bottom":"56.15%",height:"0",overflow:"hidden"}},y=["innerHTML"],w={class:"dfsdfgsd2"},x=a('<div><div class="fsdf23"><p>Design the cover image for Bandarita&#39;s channel videos</p></div></div><div class="ma25 dfdf" style=""><div class="wfelxcontectusbut" style="height:60px;margin:1px;z-index:1;border-radius:16px;display:flex;flex-direction:row;justify-content:center;position:absolute;align-items:center;background:#1f1f1f;"><a style="width:306px;display:flex;justify-content:center;" href="https://youtu.be/LFWWLvkB3mE" target="_blank" class="OURWORKbut1">see in youtube</a></div><div class="OURWORKbut dcgdfg"></div></div>',2),b={class:"dgdfg2"},_={class:"dfsdfgsd2"},k=a('<div><div class="fsdf23"><p>Design the cover image for Bandarita&#39;s channel videos</p></div></div><div class="ma25 dfdf" style=""><div class="wfelxcontectusbut" style="height:60px;margin:1px;z-index:1;border-radius:16px;display:flex;flex-direction:row;justify-content:center;position:absolute;align-items:center;background:#1f1f1f;"><a style="width:306px;display:flex;justify-content:center;" href="https://youtu.be/LFWWLvkB3mE" target="_blank" class="OURWORKbut1">see in youtube</a></div><div class="OURWORKbut"></div></div>',2),S={class:"fgffg fddq4r"},j={style:{"border-radius":"20px",position:"relative","padding-bottom":"56.15%",height:"0",overflow:"hidden"}},L=["innerHTML"],R={class:"dgdfg2"},W={class:"fgffg"},B={style:{"border-radius":"20px",position:"relative","padding-bottom":"56.15%",height:"0",overflow:"hidden"}},O=["innerHTML"],C={class:"dfsdfgsd2"},P=a('<div><div class="fsdf23"><p>Design the cover image for Bandarita&#39;s channel videos</p></div></div><div class="ma25 dfdf" style=""><div class="wfelxcontectusbut" style="height:60px;margin:1px;z-index:1;border-radius:16px;display:flex;flex-direction:row;justify-content:center;position:absolute;align-items:center;background:#1f1f1f;"><a style="width:306px;display:flex;justify-content:center;" href="https://youtu.be/LFWWLvkB3mE" target="_blank" class="OURWORKbut1">see in youtube</a></div><div class="OURWORKbut"></div></div>',2);function T(d,$,F,E,t,V){const i=c("P");return p(),f(h,null,[e("div",m,[e("div",u,[e("div",v,[e("div",{innerHTML:t.iframeString},null,8,y)])]),e("div",w,[s(i,{style:{height:"200px","line-height":"60px","font-size":"50px",width:"350px"},class:"Txet-1"},{default:o(()=>[r("Video Cover Photo")]),_:1})]),x]),e("div",b,[e("div",_,[s(i,{style:{height:"200px","line-height":"60px","font-size":"50px",width:"350px"},class:"Txet-1"},{default:o(()=>[r("Video Cover Photo")]),_:1})]),k,e("div",S,[e("div",j,[e("div",{innerHTML:t.iframeString2},null,8,L)])])]),e("div",R,[e("div",W,[e("div",B,[e("div",{innerHTML:t.iframeString3},null,8,O)])]),e("div",C,[s(i,{style:{height:"200px","line-height":"60px","font-size":"50px",width:"350px"},class:"Txet-1"},{default:o(()=>[r("Video Cover Photo")]),_:1})]),P])],64)}const J=n(g,[["render",T]]);export{J as default};
